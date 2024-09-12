017b846378c0560dd9314ff4fbacde1045c64756
himanidesai <de99hi@gmail.com>
2024-09-12 12:59:37 -0400

    Adding the categories.
    Co-authored-by: komyelizqa <komyelizqa@users.noreply.github.com>

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
+categories = ["General Knowledge", "Entertainment:Books",];
+
+let defaultContainer = document.querySelector(".category-container");
+
+for (let i=0; i< categories.length; i++) {
+    let categoryElement = document.createElement("h2");
+    categoryElement.classList.add("category_name");
+
+    categoryElement.innerText = categories[i]["category"];
+
+    defaultContainer.appendChild(categoryElement);
+}
+
 function displayQuestions(arr) {
     let commentContainer = document.querySelector(".quiz__default-quiz");
 
@@ -12,7 +25,29 @@ function displayQuestions(arr) {
         let question = document.createElement("h2");
         question.classList.add("quiz__question--name");
         question.innerText = arr[i]["question"];
-        commentContainer.appendChild(question);
-  
+        defaultContainer.appendChild(question);  
     }
 }
+
+class TriviaQuizApi {
+    constructor() {
+        this.baseUrl = "https://opentdb.com/api.php?amount=10&category=19&type=multiple";
+    }
+    async getQuiz() {
+        try {
+            const response = await axios.get(`${this.baseUrl}`);
+            const quiz = response.data;
+            // Sort comments from newest to oldest
+            return quiz;
+        } catch (error) {
+            console.error('Error fetching comments:', error);
+        }
+    }
+}
+const triviaQuizApi = new TriviaQuizApi();
+(async () => {
+    // Get questions
+    const quiz_arr =  await triviaQuizApi.getQuiz();
+    displayQuestions(quiz_arr.results);
+    console.log(quiz_arr.results);
+  })();
